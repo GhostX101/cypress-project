aaa606475ea3d7e916a5ff17dde120f7d9c6d23a
GhostX101 <endurancesolomon588@gmail.com>
2025-12-03 11:47:19 -0500

    add github action for my cypress code

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
-const { defineConfig } = require("cypress");
 
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
 module.exports = defineConfig({
   e2e: {
       WatchForFileChanges: true,
       setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
   },
+    
 });
